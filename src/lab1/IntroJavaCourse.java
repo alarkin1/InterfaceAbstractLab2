605f167b83ca202850d3ae5f77cc3c029acfe512
Alex <Alex@MajorFox>
2015-02-10 11:05:25 -0600

    Things are broken.  I am moving some stuff around and identifying general methods and variables.

---
+++ src/lab1/IntroJavaCourse.java
@@ -0,0 +1,39 @@
+package lab1;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author your name goes here
+ * @version 1.00
+ */
+public class IntroJavaCourse extends Course {
+
+    private String courseName;
+    private String prerequisites;
+
+    public IntroJavaCourse(String courseName, String courseNumber) {
+        super(courseName, courseNumber);
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    public void setCredits(double credits) {
+        if (credits < 0 || credits > 5.0) {
+            System.out.println(
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.setCredits(credits);
+    }
+
+}
